8f4dda681cc0785c4213aedc7196b747eec57996
MiShengLuLu <celina520524@outlook.com>
2021-10-14 20:08:28 +0800

    update: custom pluging and add data

--- plugins/gatsby-source-mystrapi/gatsby-node.js
+++ plugins/gatsby-source-mystrapi/gatsby-node.js
@@ -1,13 +1,27 @@
 const axios = require('axios')
 var pluralize = require('pluralize')
+const { createNodeHelpers } = require('gatsby-node-helpers')
 
-async function sourceNodes ({ actions }, configOptions) {
+async function sourceNodes ({ actions, createNodeId, createContentDigest }, configOptions) {
+  const { createNode } = actions
   const { apiURL, collectionTypes } = configOptions
   // Post -> posts   Product -> products
   const types = collectionTypes.map(type => type.toLowerCase()).map(type => pluralize(type))
 
   // 从外部数据源中获取数据
   const final = await getContents(types, apiURL)
+  // 将数据添加至 graphql 数据层中
+  for(let [key, value] of Object.entries(final)) {
+    // 1. 构建数据节点对象 allPostContent allProductContent
+    const nodeHelpers = createNodeHelpers({
+      typePrefix: key,
+      createNodeId,
+      createContentDigest
+    })
+    const createNodeObject = nodeHelpers.createNodeFactory('content')
+    // 2. 根据数据节点对象创建 graphql 数据层的数据节点
+    value.forEach(item => createNode(createNodeObject(item)))
+  }
 }
 
 async function getContents (types, apiURL) {
